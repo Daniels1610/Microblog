dea631d58747eea2bd1cf3b48a2f52b6d68fa231
Daniels1610 <danielagraz1610@hotmail.com>
2021-05-24 09:52:29 -0700

    Cambios clase 24/05/2021

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from app.models import User
+from flask_login import current_user, login_user
 
 
 @app.route('/')
@@ -24,10 +26,18 @@ def index():
 #     form = LoginForm()
 #     return render_template('login.html', title='Sign In', form=form)
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Wrong username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         flash('Login requested for user {}, remember_me{}'.format(
             form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
