67d251ba430f9ac7c74344f6f98ad10f82235be1
ProfeJerry <dudasconjerry@gmail.com>
2021-05-26 09:46:00 -0700

    Multiples cambios version Jerry

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
+from wtforms.fields.html5 import  DateField
+from wtforms.validators import DataRequired, EqualTo, Email
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +9,14 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('password2',message='The passwords must match')])
+    password2 = PasswordField('Re-enter password', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    extra = SelectField('Choose one',choices=['Yes','No'])
+    fecha_de_nacimeinto = DateField('Fecha de Nacimiento', format="%d/%m/%Y")
+    register = SubmitField('Register')
+
